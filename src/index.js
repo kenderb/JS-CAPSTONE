6168fa3b0636bc225daa36740e3e705d2da75f5c
kenderb <kenderbolivar@gmail.com>
2021-01-18 11:21:51 -0500

    Add the scenes to the index

--- src/index.js
+++ src/index.js
@@ -1,39 +1,27 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
-
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
-
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
-}
+import SceneMainMenu from './scenes/SceneMainMenu';
+import SceneMain from './scenes/SceneMain';
+import SceneGameOver from './scenes/SceneGameOver';
 
 const config = {
-    type: Phaser.AUTO,
+    type: Phaser.WEBGL,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+    width: 480,
+    height: 640,
+    backgroundColor: "black",
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { x: 0, y: 0 }
+      }
+    },
+    scene: [
+      SceneMainMenu,
+      SceneMain,
+      SceneGameOver
+    ],
+    pixelArt: true,
+    roundPixels: true
 };
 
 const game = new Phaser.Game(config);
