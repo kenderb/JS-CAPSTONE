dbbc06ed577ad2bffdc9bdcf151e2a0d3ef48aa9
kenderb <kenderbolivar@gmail.com>
2021-01-19 13:44:37 -0500

    Add title to the game

--- src/scenes/SceneMainMenu.js
+++ src/scenes/SceneMainMenu.js
@@ -1,5 +1,7 @@
 import Phaser from 'phaser';
 import sprBtnPlay from '../assets/sprBtnPlay.png';
+import sprBg0 from '../assets/sprBg0.png';
+import sprBg1 from '../assets/sprBg1.png';
 import sprBtnPlayHover from '../assets/sprBtnPlayHover.png';
 import sprBtnPlayDown from '../assets/sprBtnPlayDown.png';
 import sprBtnRestart from '../assets/sprBtnRestart.png';
@@ -7,6 +9,7 @@ import sprBtnRestartHover from '../assets/sprBtnRestartHover.png';
 import sprBtnRestartDown from '../assets/sprBtnRestartDown.png';
 import sndBtnOver from '../assets/sndBtnOver.wav';
 import sndBtnDown from '../assets/sndBtnDown.wav';
+import ScrollingBackground from '../scrollingBackground';
 
 export default class SceneMainMenu extends Phaser.Scene {
   constructor() {
@@ -14,6 +17,8 @@ export default class SceneMainMenu extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('sprBg0', sprBg0);
+    this.load.image('sprBg1', sprBg1);
     this.load.image('sprBtnPlay', sprBtnPlay);
     this.load.image('sprBtnPlayHover', sprBtnPlayHover);
     this.load.image('sprBtnPlayDown', sprBtnPlayDown);
@@ -26,6 +31,51 @@ export default class SceneMainMenu extends Phaser.Scene {
   }
 
   create() {
-    this.scene.start('SceneMain');
+    this.sfx = {
+      btnOver: this.sound.add('sndBtnOver'),
+      btnDown: this.sound.add('sndBtnDown'),
+    };
+    this.btnPlay = this.add.sprite(
+      this.game.config.width * 0.5,
+      this.game.config.height * 0.5,
+      'sprBtnPlay',
+    );
+    this.btnPlay.setInteractive();
+    this.btnPlay.on('pointerover', () => {
+      this.btnPlay.setTexture('sprBtnPlayHover');
+      this.sfx.btnOver.play();
+    }, this);
+    this.btnPlay.on('pointerout', () => {
+      this.btnPlay.setTexture('sprBtnPlay');
+    });
+    this.btnPlay.on('pointerdown', () => {
+      this.btnPlay.setTexture('sprBtnPlayDown');
+      this.sfx.btnDown.play();
+    }, this);
+    this.btnPlay.on('pointerup', () => {
+      this.btnPlay.setTexture('sprBtnPlay');
+      this.scene.start('SceneMain');
+    }, this);
+    this.title = this.add.text(this.game.config.width * 0.5, 128, 'SPACE SHOOTER', {
+      fontFamily: 'monospace',
+      fontSize: 48,
+      fontStyle: 'bold',
+      color: '#ffffff',
+      align: 'center',
+    });
+    this.title.setOrigin(0.5);
+    this.backgrounds = [];
+    for (let i = 0; i < 5; i += 1) {
+      const keys = ['sprBg0', 'sprBg1'];
+      const key = keys[Phaser.Math.Between(0, keys.length - 1)];
+      const bg = new ScrollingBackground(this, key, i * 10);
+      this.backgrounds.push(bg);
+    }
+  }
+
+  update() {
+    for (let i = 0; i < this.backgrounds.length; i += 1) {
+      this.backgrounds[i].update();
+    }
   }
 }
