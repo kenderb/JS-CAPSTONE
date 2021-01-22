222a5ca95bdd320c70c7ce074faf1a74a6532370
kenderb <kenderbolivar@gmail.com>
2021-01-22 12:07:57 -0500

    Add tests for getEnemiesByType scene

--- src/__tests__/SceneMain.test.js
+++ src/__tests__/SceneMain.test.js
@@ -2,7 +2,7 @@
 import Phaser from 'phaser';
 import SceneMain from '../scenes/SceneMain';
 
-test('createForm is a function', () => {
+test('getEnemiesByType is a function', () => {
   const mainScene = new SceneMain();
   expect(typeof mainScene.getEnemiesByType).toBe('function');
 });
